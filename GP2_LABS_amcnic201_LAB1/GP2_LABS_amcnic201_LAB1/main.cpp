ab2ca20eac5afb9da136f66596f4d4f82310d44f
amcnic201 <amcnic201@caledonian.ac.uk>
2014-09-28 20:45:45 +0100

    Back to scratch
    Started again in order to avoid lists of errors. At point now where
    window should open and close very quickly, errors involving SDL prove to
    be a problem, emailed Brian.

--- GP2_LABS_amcnic201_LAB1/GP2_LABS_amcnic201_LAB1/main.cpp
+++ GP2_LABS_amcnic201_LAB1/GP2_LABS_amcnic201_LAB1/main.cpp
@@ -1,56 +1,69 @@
 //Header Files
 #include <iostream>
+
+//Header fo SDL2 functionality
+#include <SDL.h>
+
+
+
+
+//Global Variables Go Here
+
 //Pointer to our SDL Windows
-#include <SDL h>
-
-
-
-
-//Global variables go here
-//Pomiter to our SDL Windows
 SDL_Window*window;
 
-//Constants to control the windows creation
-const int WINDOW_width = 640;
-const int WINDOW_height = 480;
+//Constant to control window creation
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
 
+
+
+//Global Functions
 
 void InitWindow(int width, int height, bool fullscreen) {
-	
-	//Create a window 
-	window = SDL - CreateWindow(
-		"Lab_1", // window title
-		SDL_WINDOWPOS_CENTERED, //x position centered
-		SDL_WINDOWPOS_CENTERED, //y position centered
-		width,					//width in pixels 
-		height,					//height in pixels 
-		SDL_WINDOW_OPENGL		//flags
-								);
+
+	//Create a window
+	window = SDL_CreateWindow(
+		"LAB 1", // window title
+		SDL_WINDOWPOS_CENTERED, // X POSITION CENTERED
+		SDL_WINDOWPOS_CENTERED, // Y POSTION CENTRED
+		width,
+		height,
+		SDL_WINDOW_OPENGL  //flags
+		);
+
+
 }
 
-//Used to Cleanup once we exit 
+//Used To CleanUp once we exit 
 
-void CleanUp(){
-
+void CleanUp()
+{
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 }
 
-// MIGHT BE MISSING A VOID CLEANUP STATEMENT HERE/ABOVE: LAB INSTRUCTION UNCLEAR ON PAGE 7 ABOUT CLEAN UP WINDOW 
+void CleanUp(){
 
-//Main Method - Entry Point
-int main(int argc, char* arg[]) {
+}
 
-	//init everything SDL, if it is non-zero we have a problem
+//Main Method-Entry Point
+int main(int argc, char*arg[]){
+
+	//init everything - SDL, if it is non zero we have a problem
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
-		return-1;
+
+		return -1;
 	}
+
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
 
-	return 0;
-}
+	CleanUp();
 
+	return 0;
+
+}
 
